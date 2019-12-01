e37fc89224115fae7025706191275c8a84f53068
devignesh <vinesh1865@gmail.com>
2019-12-01 21:19:39 +0530

    adding stuff

--- angel.js
+++ angel.js
@@ -1,2 +1,34 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
+
+(async () => {
+  const source = process.argv[2];
+  const target = process.argv[3];
+
+  const browser = await puppeteer.launch({
+    defaultViewport: {
+      height: 1080,
+      width: 1920,
+    },
+    devtools: true,
+    headless: false,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(source, {
+    waitUntil: ['networkidle0'],
+  });
+
+  try {
+    for (p = 2; p <= 20; p++) {
+      await page.click(`[data-page="${p}"]`);
+      await page.waitForSelector(`[data-page="${p + 1}"]`, {
+        timeout: 30000,
+        visible: true,
+      });
+    }
+  } catch (exception) {
+    console.log('for (...) {...}');
+    console.log(exception);
+  }
