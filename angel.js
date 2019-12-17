d47a97e27d476bb9928fba95833f90ae22443fdc
devignesh <vinesh1865@gmail.com>
2019-12-17 23:29:34 +0530

    stuff_data

--- angel.js
+++ angel.js
@@ -78,6 +78,9 @@ const puppeteer = require('puppeteer');
 	}
 	try {
 	  record.location = Array.from(value.querySelector('.location .value .tag a'),).map((value, key => {return value.textContent; }).join(',');
-	} catch (exception) {}
+	} catch (exception) {
+	  console.log('.joined')
+	  console.log('exception')
+	}
 
 
