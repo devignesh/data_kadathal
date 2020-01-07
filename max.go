12bd5ae8dba623116980f9f738a05950d7150093
devignesh <vinesh1865@gmail.com>
2020-01-07 23:20:04 +0530

    go

--- max.go
+++ max.go
@@ -34,4 +34,5 @@ func main() {
     fmt.Printf("max(%d, %d) = %d\n", x, z, max_xz)
     fmt.Printf("max(%d, %d) = %d\n", y, z, max(y, z))
     fmt.Println("min(%d, %d) = %d\n", x, y, min_xy)
+    fmt.Println("min(%d, %d) = %d\n", x, z, min_xz)
 }
