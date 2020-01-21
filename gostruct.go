aa23a0e33c4474746b3033521776baf3877da6b4
devignesh <vinesh1865@gmail.com>
2020-01-21 23:05:00 +0530

    go struct

--- gostruct.go
+++ gostruct.go
@@ -7,3 +7,9 @@ type person struct {
     name string
     age  int
 }
+func Older(p1, p2 person) (person, int) {
+    if p1.age > p2.age {
+        return p1, p1.age - p2.age
+    }
+    return p2, p2.age - p1.age
+}
