73d87f46acf51d85d94c98d5ee5f09c0954d9df3
devignesh <vinesh1865@gmail.com>
2020-01-21 23:07:34 +0530

    go

--- gostruct.go
+++ gostruct.go
@@ -24,5 +24,9 @@ func main() {
     tp_Older, tp_diff := Older(tom, paul)
     bp_Older, bp_diff := Older(bob, paul)
 
+    fmt.Printf("Of %s and %s, %s is older by %d years\n", tom.name, bob.name, tb_Older.name, tb_diff)
+    fmt.Printf("Of %s and %s, %s is older by %d years\n", tom.name, paul.name, tp_Older.name, tp_diff)
+    fmt.Printf("Of %s and %s, %s is older by %d years\n", bob.name, paul.name, bp_Older.name, bp_diff)
+
 
 }
