075a95e7f3cc045fa2029a01765b54eb18c2fd0a
devignesh <vinesh1865@gmail.com>
2020-02-06 23:18:44 +0530

    gostuff

--- gostruct.go
+++ gostruct.go
@@ -14,9 +14,9 @@ func Older(p1, p2 person) (person, int) {
     return p2, p2.age - p1.age
 }
 func main() {
-    var tom person
+    var tamil person
 
-    tom.name, tom.age = "vicky", 18
+    tamil.name, tamil.age = "vicky", 18
     bob := person{age: 22, name: "suji"}
     paul := person{"vj", 24}
 
@@ -24,8 +24,8 @@ func main() {
     tp_Older, tp_diff := Older(tom, paul)
     bp_Older, bp_diff := Older(bob, paul)
 
-    fmt.Printf("Of %s and %s, %s is older by %d years\n", tom.name, bob.name, tb_Older.name, tb_diff)
-    fmt.Printf("Of %s and %s, %s is older by %d years\n", tom.name, paul.name, tp_Older.name, tp_diff)
+    fmt.Printf("Of %s and %s, %s is older by %d years\n", tamil.name, bob.name, tb_Older.name, tb_diff)
+    fmt.Printf("Of %s and %s, %s is older by %d years\n", tamil.name, paul.name, tp_Older.name, tp_diff)
     fmt.Printf("Of %s and %s, %s is older by %d years\n", bob.name, paul.name, bp_Older.name, bp_diff)
 
 
